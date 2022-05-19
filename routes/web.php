3a616b5381a0faab20410a650218787a59da143e
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-05-19 23:59:45 +0700

    add kategori

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('kategori', KategoriController::class);
+Route::get('/search', [KategoriController::class, 'search'])->name('search');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
