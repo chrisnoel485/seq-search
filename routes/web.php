46c0e9508ed27316991944266a754c6f7595a468
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-05-22 18:31:55 +0700

    add Login

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,5 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/search', [KategoriController::class, 'search'])->name('search');
     Route::get('/search', [LetakController::class, 'search'])->name('search');
     Route::get('/search', [AsetController::class, 'search'])->name('search'); 
+    Route::get('/home', 'HomeController@index')->name('home');
 });
