2f14d459490c972ae5189c2f3528e26055f29be0
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-05-22 23:16:58 +0700

    add role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -14,5 +14,22 @@ class RoleController extends Controller
         return view('role.index', compact('role'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
-    
+    public function create()
+    {
+        //
+        return view('role.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        //fungsi eloquent untuk menambah data
+        Role::create($request->all());
+
+        //jika data berhasil ditambahkan, akan kembali ke halaman utama
+        return redirect()->route('role.index')
+            ->with('success', 'Merek Berhasil Ditambahkan');
+    }
 }
