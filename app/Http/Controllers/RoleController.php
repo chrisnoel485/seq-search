6bc1bdfa852f5454e7c0bf3d4d7c829e66acaedd
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-05-22 23:08:41 +0700

    add role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -10,7 +10,7 @@ class RoleController extends Controller
     //
     public function index()
     {
-        $role = Role::::latest()->paginate(10);
+        $role = Role::latest()->paginate(10);
         return view('role.index', compact('role'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
