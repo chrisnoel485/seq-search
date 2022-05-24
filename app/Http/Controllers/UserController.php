8005992f0d05de3182c4e69bb843dab01e9ad5bf
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-05-24 22:46:15 +0700

    add role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -73,4 +73,10 @@ class UserController extends Controller
         return redirect()->route('letak.index')
             ->with(['success' => 'User Berhasil Dihapus']);
     }
+    public function show($id)
+    {
+       // User::find($id)->delete();
+        //return redirect()->route('letak.index')
+        //    ->with(['success' => 'User Berhasil Dihapus']);
+    }
 }
