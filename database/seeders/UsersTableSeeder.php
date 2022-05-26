8126ad9c3227c297301b1d837e84f7ce50245200
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-05-26 23:19:43 +0700

    add role

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class UsersTableSeeder extends Seeder
 {
@@ -20,5 +22,12 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('admin'),
             'status' => true
         ]);
+        $role = Role::create(['name' => 'Admin']);
+     
+        $permissions = Permission::pluck('id','id')->all();
+   
+        $role->syncPermissions($permissions);
+     
+        $user->assignRole([$role->id]);
     }
 }
