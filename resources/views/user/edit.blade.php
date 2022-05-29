aef6b414bd26b349e47a5219cf9d22f9e8c4f591
chrisnoel485 <chrisnoel485@gmail.com>
2022-05-29 20:42:27 +0700

    add role

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -76,12 +76,16 @@
                                             </select>
                                         </div>
                                         <div class="form-group">
-                                            <label for="">Role</label>
-                                            @foreach ($roles as $row)
-                                            <input type="radio" name="role" 
-                                                {{ $user->hasRole($row) ? 'checked':'' }}
-                                                value="{{ $row }}"> {{  $row }} <br>
-                                            @endforeach
+                                            <div>
+                                                <label for="">Role</label>
+                                            </div>    
+                                            <div>
+                                                @foreach ($roles as $row)
+                                                <input type="radio" name="role" 
+                                                   {{ $user->hasRole($row) ? 'checked':'' }}
+                                                    value="{{ $row }}"> {{  $row }} <br>
+                                                @endforeach
+                                            </div>
                                         </div>
                                         <div class="card-footer">
                                             <a href="{{ URL::to('user') }}" class="btn btn-outline-info">Kembali</a>
