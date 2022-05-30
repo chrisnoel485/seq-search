f430f736923ce5d9b1bd70704a053d98e64a0a17
chrisgokomodo <chris.noel@gokomodo.com>
2022-05-30 20:29:23 +0700

    add role

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -20,53 +20,79 @@
             </div>
         </div>
     </div>
-    <div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Role Management</h2>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div>
+                                <h3 class="card-title">Data Role</h3>
+                            </div>
+                            <div class="card-tools">
+                                @can('role-create')
+                                <a href="/role/create" class="btn btn-primary">Input Role Baru</a>
+                                @endcan
+                            </div>
+                        </div>
+                        <div class="card-header">
+                            <form class="form" method="get" action="{{ route('search') }}">
+                                <div class="form-group w-100 mb-3">
+                                    <label for="search" class="d-block mr-2">Pencarian</label>
+                                    <input type="text" name="search" class="form-control w-75 d-inline" id="search" placeholder="Masukkan keyword">
+                                    <button type="submit" class="btn btn-primary mb-1">Cari</button>
+                                </div>
+                            </form>
+                        </div>
+                        @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
+                        @endif
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <table class="table table-bordered table-hover">
+                                        <tr>
+                                            <th class="text-center">No</th>
+                                            <th class="text-center">Role</th>
+                                            <th class="text-center">Guard</th>
+                                            <th class="text-center">Created At</th>
+                                            <th width="280px" class="text-center">Action</th>
+                                        </tr>
+                                        @foreach ($roles as $key => $role)
+                                        <tr>
+                                            <td>{{ ++$i }}</td>
+                                            <td>{{ $role->name }}</td>
+                                            <td>{{ $role->guard_name }}</td>
+                                            <td>{{ $role->created_at }}</td>
+                                            <td>
+                                                <form action="{{ route('role.destroy',$role->id) }}" method="POST">
+                                                    <a class="btn btn-info" href="{{ route('role.show',$role->id) }}">Show</a>
+                                                    @can('role-edit')
+                                                    <a class="btn btn-primary" href="{{ route('role.edit',$role->id) }}">Edit</a>
+                                                    @endcan
+                                                    @csrf
+                                                    @can('role-delete')
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                    @endcan
+                                                </form>
+                                            </td>
+                                        </tr>
+                                        @empty
+                                        <tr>
+                                            <td colspan="4" class="text-center">Tidak ada data</td>
+                                        </tr>
+                                        @endforeach
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="pull-right">
-        @can('role-create')
-            <a class="btn btn-success" href="{{ route('role.create') }}"> Create New Role</a>
-            @endcan
-        </div>
-    </div>
-</div>
-
-
-@if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
-@endif
-
-
-<table class="table table-bordered">
-  <tr>
-     <th>No</th>
-     <th>Name</th>
-     <th width="280px">Action</th>
-  </tr>
-    @foreach ($role as $key => $role)
-    <tr>
-        <td>{{ ++$i }}</td>
-        <td>{{ $role->name }}</td>
-        <td>
-            <a class="btn btn-info" href="{{ route('role.show',$role->id) }}">Show</a>
-            @can('role-edit')
-                <a class="btn btn-primary" href="{{ route('role.edit',$role->id) }}">Edit</a>
-            @endcan
-            @can('role-delete')
-                {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                {!! Form::close() !!}
-            @endcan
-        </td>
-    </tr>
-    @endforeach
-</table>
-
-
-{!! $role->render() !!}
+    </section>
 </div>
 @stop
