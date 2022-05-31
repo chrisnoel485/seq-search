94ec641f9993c573eec8e1944d6cf4d5866161ac
root <root@bastion-sequispay.asia-southeast2-a.c.gcp-shared-host-nonprod-276203.internal>
2022-06-01 00:10:16 +0700

    add posisi

---
+++ app/Http/Controllers/AsetposisiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Asetposisi;
+use Illuminate\Http\Request;
+
+class AsetposisiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Asetposisi  $asetposisi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Asetposisi $asetposisi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Asetposisi  $asetposisi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Asetposisi $asetposisi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Asetposisi  $asetposisi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Asetposisi $asetposisi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Asetposisi  $asetposisi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Asetposisi $asetposisi)
+    {
+        //
+    }
+}
