33a5afedcfbcdc6fd65f402adc563b0a0816ff2e
chrisnoel485 <chrisnoel485@gmail.com>
2022-06-01 23:08:43 +0700

    fix role

--- app/Http/Controllers/PosisiController.php
+++ app/Http/Controllers/PosisiController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Anggota;
+use App\Models\Hadiah;
 
 class PosisiController extends Controller
 {
     //
+    public function index()
+    {
+    	$anggota = Anggota::get();
+    	return view('posisi.index', ['anggota' => $anggota]);
+    }
+
 }
