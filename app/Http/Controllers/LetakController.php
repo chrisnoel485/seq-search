84ab08fb479a07cad1e5e0e9c7db9b037f6d442f
chrisgokomodo <chris.noel@gokomodo.com>
2022-06-01 18:40:27 +0700

    Revert "add aset_posisi"
    This reverts commit 2a2c57592493bcead6da0c741e1510bb46851617.

--- app/Http/Controllers/LetakController.php
+++ app/Http/Controllers/LetakController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Letak;
 use App\Models\Kategori;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Input;
 
 class LetakController extends Controller
 {
