4ff340e27fddca34343d174ae9713104495249cc
chrisgokomodo <chris.noel@gokomodo.com>
2022-06-03 18:06:30 +0700

    add posisi

--- resources/views/posisi/index.blade.php
+++ resources/views/posisi/index.blade.php
@@ -31,10 +31,10 @@
                             </div>
                         </div>
                         <div class="card-header">
-                            <form class="form" method="get" action="{{ route('search') }}">
+                            <form class="form" method="get" action="{{ route('searchposisi') }}">
                                 <div class="form-group w-100 mb-3">
-                                    <label for="search" class="d-block mr-2">Pencarian</label>
-                                    <input type="text" name="search" class="form-control w-75 d-inline" id="search" placeholder="Masukkan keyword">
+                                    <label for="searchposisi" class="d-block mr-2">Pencarian</label>
+                                    <input type="text" name="searchposisi" class="form-control w-75 d-inline" id="searchposisi" placeholder="Masukkan keyword">
                                     <button type="submit" class="btn btn-primary mb-1">Cari</button>
                                 </div>
                             </form>
