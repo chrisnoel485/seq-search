9992fd127025ac66651c9cc3500656a5c773022e
chrisgokomodo <chris.noel@gokomodo.com>
2022-06-08 19:40:17 +0700

    fix ui

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -49,10 +49,10 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="{{ route('aset.index') }}" class="nav-link">Aset</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="{{ route('posisi.index') }}" class="nav-link">History Aset</a>
       </li>
     </ul>
 
@@ -60,9 +60,6 @@
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
       <li class="nav-item">
-        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-          <i class="fas fa-search"></i>
-        </a>
         <div class="navbar-search-block">
           <form class="form-inline">
             <div class="input-group input-group-sm">
