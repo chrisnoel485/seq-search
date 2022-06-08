24de13452c49efa2a7075596330703793cdb0233
chrisgokomodo <chris.noel@gokomodo.com>
2022-06-08 19:36:41 +0700

    fix ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,12 +30,12 @@
                         <div class="small-box bg-info">
                             <div class="inner">
                                 <h3>0</h3>
-                                <p>Products</p>
+                                <p>Aset</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-bag"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                            <a href="{{ route('aset.index') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
 
@@ -43,12 +43,12 @@
                         <div class="small-box bg-success">
                             <div class="inner">
                                 <h3>0<sup style="font-size: 20px">%</sup></h3>
-                                <p>Orders</p>
+                                <p>History Aset</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-stats-bars"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                            <a href="{{ route('posisi.index') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-6">
