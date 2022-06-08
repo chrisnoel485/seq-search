34ebd20bd21acd17d2d8c95e607312b2289b034a
chrisgokomodo <chris.noel@gokomodo.com>
2022-06-08 21:19:09 +0700

    fix ui

--- resources/views/role/show.blade.php
+++ resources/views/role/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('title')
-    <title>Daftar Kategori</title>
+    <title>Daftar Role</title>
 @endsection
 
 @section('content')
@@ -9,7 +9,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">                      
-                    <h1 class="m-0 text-dark">Manajemen Kategori</h1>
+                    <h1 class="m-0 text-dark">Manajemen Role</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
