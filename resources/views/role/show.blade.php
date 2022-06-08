500f0841e4a7df4e15536679d478077a8fbb89de
chrisgokomodo <chris.noel@gokomodo.com>
2022-06-08 21:18:27 +0700

    fix ui

--- resources/views/role/show.blade.php
+++ resources/views/role/show.blade.php
@@ -27,28 +27,10 @@
                     <div class="card">
                         <div class="card-header">
                             <div>
-                                <h3 class="card-title">Detail Kategori</h3>
+                                <h3 class="card-title">Detail Role</h3>
                             </div>
                         </div>
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <strong>Name:</strong>
-                                        {{ $role->name }}
-                                    </div>
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <strong>Permissions:</strong>
-                                        @if(!empty($rolePermissions))
-                                            @foreach($rolePermissions as $v)
-                                                <label class="label label-success">{{ $v->name }},</label>
-                                            @endforeach
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
                             <div class="row">
                                 <div class="col-md-12">
                                     <ul class="list-group list-group-flush">
